9d9a8acc138f9a0af8faca80e88355623e62a470
huchunxu <huchunxu@hust.edu.cn>
2017-09-06 15:23:22 +0800

    Add smach_tutorials

---
+++ smach_tutorials/scripts/user_data.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+
+# define state Foo
+class Foo(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=['outcome1','outcome2'],
+                             input_keys=['foo_counter_in'],
+                             output_keys=['foo_counter_out'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state FOO')
+        if userdata.foo_counter_in < 3:
+            userdata.foo_counter_out = userdata.foo_counter_in + 1
+            return 'outcome1'
+        else:
+            return 'outcome2'
+
+
+# define state Bar
+class Bar(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=['outcome1'],
+                             input_keys=['bar_counter_in'])
+        
+    def execute(self, userdata):
+        rospy.loginfo('Executing state BAR')
+        rospy.loginfo('Counter = %f'%userdata.bar_counter_in)        
+        return 'outcome1'
+        
+
+def main():
+    rospy.init_node('smach_example_state_machine')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['outcome4'])
+    sm.userdata.sm_counter = 0
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('FOO', Foo(), 
+                               transitions={'outcome1':'BAR', 
+                                            'outcome2':'outcome4'},
+                               remapping={'foo_counter_in':'sm_counter', 
+                                          'foo_counter_out':'sm_counter'})
+        smach.StateMachine.add('BAR', Bar(), 
+                               transitions={'outcome1':'FOO'},
+                               remapping={'bar_counter_in':'sm_counter'})
+
+    # Create and start the introspection server
+    sis = smach_ros.IntrospectionServer('my_smach_introspection_server', sm, '/SM_ROOT')
+    sis.start()
+    
+    # Execute SMACH plan
+    outcome = sm.execute()
+    
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
